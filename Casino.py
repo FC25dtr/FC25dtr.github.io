f15ca2819fc440a83588e24b916965e9efc6534d
FC25dtr <tonin.pacher@gmail.com>
2025-05-01 21:07:17 +0200

    Create Casino.py
    j'essaie de créer un interface graphique type casino en python avec l'aide de tkinter. Porjet réalisé hors parcours scolaire

---
+++ Casino.py
@@ -0,0 +1,120 @@
+import tkinter as tk 
+
+#fenetre de base
+Page_garde = tk.Tk()
+Page_garde.title("Casino Game")
+Page_garde.geometry("1920x1080")
+Page_garde.config(bg="white")
+
+Gros_titre = tk.Label(Page_garde, text="BIENVENUE SUR LE CASINO GAME")
+Gros_titre.config(font=("Calibri", 30))  
+Gros_titre.config(bg="#6A5ACD", fg="white")  
+Gros_titre.config(width=85, height=5)
+Gros_titre.place(x=0, y=0)
+
+regles = tk.Label(Page_garde, text="Pour afficher les regles appuyez sur la touche A du clavier")
+regles.config(font=("Calibri", 15))  
+regles.config(bg="white", fg="black")
+regles.place(x=500, y=250)
+
+from tkinter import messagebox
+def affiche_règles(a):
+    messagebox.showinfo("Règles", "Bonjour, bienvenue sur le casino en ligne réalisé par Tonin pour découvrir tkinter, dans ce casino tu trouveras trois mini jeux : pile ou face, les gobelets, chiffre aléatoire. Clique sur le bouton du jeu pour le lancer. Pour afficher les proba des jeux appuis sur H")
+Page_garde.bind("a", affiche_règles)
+
+choix_jeu = tk.Label(Page_garde, text="Choisi ton mini jeu :")
+choix_jeu.config(font=("Calibri", 30))  
+choix_jeu.config(bg="white", fg="black")
+choix_jeu.place(x=600, y=350)
+
+
+def valider1():
+    global entree1
+    global frame_jeu1
+    frame_jeu1 = tk.Frame(Page_garde, width=2000, height=2000)
+    frame_jeu1.pack_propagate(False)
+    frame_jeu1.config(bg="#FADADD") 
+    frame_jeu1.pack()
+    Gros_titre = tk.Label(Page_garde, text="JEU DE LA PIECE")
+    Gros_titre.config(font=("Calibri", 30))  
+    Gros_titre.config(bg="#E1AD01", fg="white")  
+    Gros_titre.config(width=85, height=5)
+    Gros_titre.place(x=0, y=0)
+    demande_age = tk.Label(frame_jeu1, text="Ecris ton choix en lettre minuscule :",font=("Arial",20))
+    demande_age.config(bg="#FADADD")
+    demande_age.place(x=570,y=300)
+    entree1 = tk.Entry(frame_jeu1, font=("Calibri", 30))
+    entree1.place(x=600,y=400)
+    bouton = tk.Button(frame_jeu1, text="Valider", command=control_jeu1)
+    bouton.config(width=30, height=2)
+    bouton.place(x=670,y=500)
+
+import random
+
+def control_jeu1():
+    global entree1
+    texte = entree1.get()
+    choix = random.choice(["pile", "face"])
+    if texte == choix:
+        gagne = tk.Label(frame_jeu1,text="Félicitation tu as gagné")
+        gagne.config(bg="#FADADD")
+        gagne.config(font=("Calibri", 30)) 
+        gagne.place(x=580,y=600)
+    else:
+        perdu = tk.Label(frame_jeu1,text="Dommage tu as perdu retente ta chance")
+        perdu.config(bg="#FADADD")
+        perdu.config(font=("Calibri", 30)) 
+        perdu.place(x=580,y=600)
+    
+def valider2():
+    frame_jeu2 = tk.Frame(Page_garde, width=2000, height=2000)
+    frame_jeu2.pack_propagate(False)
+    frame_jeu2.config(bg="#FADADD") 
+    frame_jeu2.pack()
+    
+def valider3():
+    frame_jeu3 = tk.Frame(Page_garde, width=2000, height=2000)
+    frame_jeu3.pack_propagate(False)
+    frame_jeu3.config(bg="#FADADD") 
+    frame_jeu3.pack()
+    
+#FF6600
+bouton_jeu1 = tk.Button(Page_garde, text="PILE OU FACE", width=50, height=7, command=valider1)
+bouton_jeu1.place(x=200, y=550)
+bouton_jeu1.config(bg="#E1AD01")
+bouton_jeu2 = tk.Button(Page_garde, text="GOBELET", width=50, height=7, command=valider2)
+bouton_jeu2.place(x=600, y=550)
+bouton_jeu2.config(bg="#E1AD01")
+bouton_jeu3 = tk.Button(Page_garde, text="ALEA CHIFFRE", width=50, height=7, command=valider3)
+bouton_jeu3.place(x=1000, y=550)
+bouton_jeu3.config(bg="#E1AD01")
+
+#frame verif age 
+frame_age = tk.Frame(Page_garde, width=2000, height=2000)
+frame_age.pack_propagate(False)
+frame_age.config(bg="#FADADD") 
+frame_age.pack()
+
+frame_age.lift()
+
+#verif de l'age entry
+def valider():
+    frame_age.pack_forget()
+    
+def controle():
+    texte = entry.get()
+    if int(texte) >= 18:
+        valider()
+    else:
+        tk.Label(frame_age, text="Le Casino est interdit au moins de 18 ans",bg="#FADADD", fg="red").pack()
+demande_age = tk.Label(frame_age, text="ENTREZ VOTRE AGE :",font=("Arial",20))
+demande_age.config(bg="#FADADD")
+demande_age.pack(pady=30)
+entry = tk.Entry(frame_age, font=("Calibri", 14))
+entry.pack(pady=10)
+bouton = tk.Button(frame_age, text="Valider", command=controle)
+bouton.pack(pady=30)
+
+#affichage texte page principale
+
+Page_garde.mainloop()
